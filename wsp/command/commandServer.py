bc821e654981a14c5001f69219f3231e56ce6831
nlourie <nlourie@sas.upenn.edu>
2020-02-03 18:01:19 -0500

    made command server exit nicely on interrupt

--- wsp/command/commandServer.py
+++ wsp/command/commandServer.py
@@ -26,57 +26,62 @@ def printphrase(phrase = 'default phrase'):
 
 
 def start_commandServer(addr = 'localhost', port = 7075):
-    # Create a TCP/IP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    # associate the socket with a server address: Bind to an address
-    server_address = (addr, port)
-    
-    print(f'starting up on {server_address[0]} port {server_address[1]}')
-    sock.bind(server_address)
-    
-    # listen for incoming connections
-    sock.listen(5) # the number is the number of bad connections it allows before refusing connection
-    
-    while True:
-        # wait for a connection
-        print('Waiting for a connection')
-        client_socket, client_address = sock.accept()
+    try:# Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        try:
-            print(f'connection from IP {client_address[0]} | port  {client_address[1]}')
+        # associate the socket with a server address: Bind to an address
+        server_address = (addr, port)
+        
+        print(f'starting up on {server_address[0]} port {server_address[1]}')
+        sock.bind(server_address)
+        
+        # listen for incoming connections
+        sock.listen(5) # the number is the number of bad connections it allows before refusing connection
+        
+        while True:
+            # wait for a connection
+            print('Waiting for a connection')
+            client_socket, client_address = sock.accept()
             
-            # receive the data in small chunks and retransmit it
-            while True:
-                cmd = client_socket.recv(1024)
-                print(f'received: {cmd.decode("utf-8")}')
-                cmd_txt = cmd.decode("utf-8")
-                if cmd:
-                    #reply = f'received command: {cmd}\n'
-                    #client_socket.send(bytes(reply,"utf-8"))
-                    if cmd_txt.lower() == 'killserver':
-                        client_socket.close()
-                        sock.close()
-                        sys.exit()
+            try:
+                print(f'connection from IP {client_address[0]} | port  {client_address[1]}')
+                
+                # receive the data in small chunks and retransmit it
+                while True:
+                    cmd = client_socket.recv(1024)
+                    print(f'received: {cmd.decode("utf-8")}')
+                    cmd_txt = cmd.decode("utf-8")
+                    if cmd:
+                        #reply = f'received command: {cmd}\n'
+                        #client_socket.send(bytes(reply,"utf-8"))
+                        if cmd_txt.lower() == 'killserver':
+                            client_socket.close()
+                            sock.close()
+                            sys.exit()
+                        else:
+                            try:
+                                # try to evaluate the command
+                                result = eval(cmd_txt)
+                                reply = f'command [{cmd_txt}] executed, result = [{result}]'
+                                client_socket.send(bytes(reply,"utf-8"))
+                            except:
+                                reply = f'command [{cmd_txt}] not executed properly: \n enter "quit" to stop client session or "killserver" to stop command server'
+                                client_socket.send(bytes(reply,"utf-8"))
                     else:
-                        try:
-                            # try to evaluate the command
-                            result = eval(cmd_txt)
-                            reply = f'command [{cmd_txt}] executed, result = [{result}]'
-                            client_socket.send(bytes(reply,"utf-8"))
-                        except:
-                            reply = f'command [{cmd_txt}] not executed properly: \n enter "quit" to stop client session or "killserver" to stop command server'
-                            client_socket.send(bytes(reply,"utf-8"))
-                else:
-                    print(f'no more data from {client_address}')
-                    break
-        
-        finally:
-            # clean up and close the connection. the try:finally thing makes it close even if there's an error
-            print('closing socket.')
-            client_socket.close()
+                        print(f'no more data from {client_address}')
+                        break
+            finally:
+                # clean up and close the connection. the try:finally thing makes it close even if there's an error
+                print('closing socket.')
+                client_socket.close()
+    except KeyboardInterrupt:
+        # This makes it exit gracefully if you do cntl + c by leaving the loop and proceeding down to close the server socket
+        pass
+    # now that you've keyboardinterrupted, close the server socket
+    sock.close()
 
 if __name__ == '__main__':
     
-    start_commandServer(addr = 'localhost',port = 7075)
+    start_commandServer(addr = 'localhost',port = 7007)
     
