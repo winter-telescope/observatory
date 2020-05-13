bfc4680a26a1fa82d3c0ef147cc10bce8b92cb8a
Danielle Frostig <frostig@mit.edu>
2020-05-13 16:38:51 -0400

    switch queues at top of night

--- scheduler/winter_scheduler/winter_sim/simulate.py
+++ scheduler/winter_scheduler/winter_sim/simulate.py
@@ -97,24 +97,25 @@ def simulate(scheduler_config_file, sim_config_file,
             block_use = scheduler.find_block_use_tonight(
                               tel.current_time)
             timed_obs_count = scheduler.count_timed_observations_tonight()
+            
+            try: # W
+                scheduler.queues['default'].assign_nightly_requests(
+                        tel.current_state_dict(),
+                        scheduler.obs_log, block_use = block_use,
+                        timed_obs_count = timed_obs_count, time_limit = time_limit)
+                current_night_mjd = np.floor(tel.current_time.mjd)
+                # log pool stats
+                logger.info(calc_pool_stats(
+                    scheduler.queues['default'].rp.pool, intro="Nightly requests initialized"))
 
-            scheduler.queues['default'].assign_nightly_requests(
-                    tel.current_state_dict(),
-                    scheduler.obs_log, block_use = block_use,
-                    timed_obs_count = timed_obs_count, time_limit = time_limit)
-            current_night_mjd = np.floor(tel.current_time.mjd)
-            # log pool stats
-            logger.info(calc_pool_stats(
-                scheduler.queues['default'].rp.pool, intro="Nightly requests initialized"))
+            if tel.check_if_ready():
+                current_state = tel.current_state_dict()
 
-        if tel.check_if_ready():
-            current_state = tel.current_state_dict()
+                #scheduler.check_for_TOO_queue_and_switch(current_state['current_time'])
+                scheduler.check_for_timed_queue_and_switch(current_state['current_time'])
 
-            #scheduler.check_for_TOO_queue_and_switch(current_state['current_time'])
-            scheduler.check_for_timed_queue_and_switch(current_state['current_time'])
-
-            # get coords
-            try:
+                # get coords
+            #try:
                 next_obs = scheduler.Q.next_obs(current_state, 
                         scheduler.obs_log)
                  # W, very hacked together re-index
