dc77d9f311bc12ad0ee9e18de638a67100f8bb03
Danielle Frostig <frostig@mit.edu>
2021-06-09 18:06:29 -0400

    Add viscam stuff to housekeeping

--- wsp/housekeeping/housekeeping.py
+++ wsp/housekeeping/housekeeping.py
@@ -41,7 +41,7 @@ from housekeeping import labjacks
 # the main housekeeping class it lives in the namespace of the control class
 
 class housekeeping():                     
-    def __init__(self, config, base_directory, mode = None, schedule = None, telescope = None, dome = None, weather = None, chiller = None, pdu1 = None, counter = None, ephem = None):            
+    def __init__(self, config, base_directory, mode = None, schedule = None, telescope = None, dome = None, weather = None, chiller = None, pdu1 = None, counter = None, ephem = None, viscam = None):
         
         
         # store the config
@@ -60,6 +60,7 @@ class housekeeping():
         self.pdu1 = pdu1
         self.counter = counter
         self.ephem = ephem
+        self.viscam = viscam
         
         # setup any labjacks that are in the config
         '''
