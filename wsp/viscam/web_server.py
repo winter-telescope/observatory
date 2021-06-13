2fdb216afa666d977a88694d58f772fa298b4f58
Danielle Frostig <frostig@mit.edu>
2021-06-13 01:29:28 -0400

    get raspi web server hooked up again

--- wsp/viscam/web_server.py
+++ wsp/viscam/web_server.py
@@ -11,12 +11,31 @@ from flask import Flask
 from flask import request
 from flask.json import jsonify
 import serial # pip install pyserial
-
+import time
 app = Flask(__name__)
 
 # https://stackoverflow.com/questions/19277280/preserving-global-state-in-a-flask-application
 shutter_state = {}
-shutter_state['foo'] = -1
+shutter_state['foo'] = 3
+
+class FilterWheel():
+    # initialize
+    def __init__(self):
+        self.ser = serial.Serial('/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0', baudrate=9600)
+        #self.ser = serial.Serial('/dev/tty.usbserial-0001', baudrate=9600)
+        time.sleep(25)
+    
+    def filterwheel_command(self, command):
+        self.ser.write(command)
+        time.sleep(1)
+        reply = self.ser.read(self.ser.inWaiting())
+        return reply 
+
+try:
+    FW = FilterWheel()
+except:
+    print('Exception: no filter wheel plugged in on start up')
+
 
 # check raspi/web server responsiveness
 @app.route('/check_raspi', endpoint='func1', methods=['GET'])
@@ -29,41 +48,39 @@ def check_raspi():
 def get_filter_command():
     # open serial port
     try:
-        ser = serial.Serial('/dev/tty.usbserial-0001')
-    
+        #ser = serial.Serial('/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0')
+        #time.sleep(25)
         # get web request
         n = request.args.get("n")
         n = int(n)
-        reply_string = ""
+        
         if (n == 1 or n == 2 or n == 3 or n == 4 or n == 5 or n == 6 or n == 7):
             # command
             command = str(n-1) # make string
             command = command.encode() # make bytes
-            ser.write(command)
+            ret = FW.filterwheel_command(command)
             reply_string = "Set filter wheel position to " + str(n)
         elif (n == 8):
             # command 
-            ser.write(b'NOW')
-            pos = ser.read()
-            reply_string = "Current position is " + str(pos)
+            command = b'NOW'
+            ret = FW.filterwheel_command(command)
+            reply_string = "Current position is " + str(ret)
         elif (n == 9):
             # command 
-            ser.write(b'MXP')
-            n_pos = ser.read()
-            reply_string = "Number of available positions:  " + str(n_pos)
+            command = b'MXP'
+            ret = FW.filterwheel_command(command)
+            reply_string = "Number of available positions:  " + str(ret)
         elif (n == 10):
             # command 
-            ser.write(b'VRS')
-            ver = ser.read()
-            reply_string = "Filter wheel firmware version:  " 
-            + str(ver.decode('ascii')) + "\n Firmware should be version 2. If not, see " 
-            + "https://www.qhyccd.com/index.php?m=content&c=index&a=show&catid=68&id=181"
+            command = b'MXP'
+            ret = FW.filterwheel_command(command)
+            reply_string = "Filter wheel firmware version:  " +  str(ret.decode('ascii')) + "\n Firmware should be version 2. If not, see https://www.qhyccd.com/index.php?m=content&c=index&a=show&catid=68&id=181"
         else:
             reply_string = "Not a valid command"
         
         return reply_string
     except:
-        reply_string = "Port could not be opened"
+        reply_string = "Port could not be opened, try restarting the pi"
         return reply_string, 403
 
 
@@ -75,7 +92,7 @@ def get_shutter_command():
     # https://www.uniblitz.com/wp-content/uploads/2020/10/VED24-User-Manual-1.41_Rev_B12.pdf
     
     try:
-        ser = serial.Serial('/dev/tty.usbserial-A146VRZG', baudrate=9600, bytesize=8, parity='N')
+        ser = serial.Serial('/dev/serial/by-id/usb-FTDI_MM232R_USB__-__Serial_A146VRZG-if00-port0', baudrate=9600, bytesize=8, parity='N')
     
         n = request.args.get("open")
         n = int(n)
