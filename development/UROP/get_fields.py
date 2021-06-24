6afe5d5447fce22f321d8bba2cac46f3df364e24
Abigail Schipper <abbies@mit.edu>
2021-06-24 03:31:13 -0400

    Update get_fields.py

--- development/UROP/get_fields.py
+++ development/UROP/get_fields.py
@@ -1,61 +1,78 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 21 13:39:00 2021
-
-@author: Abigail Schipper
-"""
-
 import pandas as pd
+import requests
 import astropy.units as u
 from astropy.coordinates import Angle
-import requests
 
-def fieldFinder(RA,Dec,decimal=False,sigma=0.13009,file='SUMMER_fields.txt',names=['ID',"RA",'Dec','EBV','GalLong','GalLat','EclLong','EclLat']):
+def get_fields(url=None,rochester=False,RAs=None,Decs=None,sigma=0.13009,file='SUMMER_fields.txt',names=['ID',"RA",'Dec','EBV','GalLong','GalLat','EclLong','EclLat']):
     '''
+    
+    Either have a url with the RA/Decs, or input your own through RAs and Decs.
     Parameters
     ----------
-    RA: Float
-        Right ascension in decimal form
-    Dec: Float
-        Declination in decimal form
-    decimal: Boolean
-        True if RA/Dec in decimal form. False if RA/Dec in 00h00m00s/00d00m00s form (default)
-    Sigma: FLoat
-        Distance that the field's Dec can be from desired Dec. Defaults to 13.009
-    File: String
+    url : String
+        A URL of the supernovae from a csv in https://sites.astro.caltech.edu/ztf/bts/explorer.php?f=s&subsample=sn&classstring=Ia&quality=y&purity=y&ztflink=lasair&startsavedate=2021-05-31&endsavedate=2021-06-15&endpeakmag=19.0&reverse=y.RIPTION.
+        or https://www.rochesterastronomy.org/sn2021/snlocations.html
+        Defaults to none if you have no url
+    RAs: List of strings
+        List of RAs in form 00h00m00s. Defaults to None. Leave as none if using a url for RA/Decs
+    Decs: List of strings
+        List of Decs in form 00d00m00s. Defaults to None. Leave as none if using a url for RA/Decs
+    sigma : FLoat, optional
+        Distance that the field's Dec can be from desired Dec. Defaults to 0.13009
+    file : String, optional
         File name of file with fields. Defaults to "SUMMER_fields.txt
-    Names: List of strings
+    names : List of strings, optional
         Names of columns. Defaults to ['ID',"RA",'Dec','EBV','GalLong','GalLat','EclLong','EclLat'])
-    
+
+    Returns
+    -------
+    fields : List of integers
+        Field IDs of each RA/Dec
+
+    '''
+    data = pd.read_fwf("SUMMER_fields.txt")
+    df = pd.DataFrame(data)
+    if RAs==None and Decs==None:
+        if not rochester:
+            RAs,Decs=getRADecs(url)
+        else:
+            RAs,Decs=get_html_panda(url)
+    fields=[]
+    for i in range(len(RAs)):
+        fields.append(GetFieldNum(RAs[i], Decs[i], df))
+    return fields
+        
+def GetFieldNum(ra, dec, fieldNums):
+    '''
+
+    Parameters
+    ----------
+    ra : STRING
+        Right ascension of body you wish to observe in format '00h00m00.00s'
+    Dec : STRING
+         Declination of body you wish to observe in format '00d00m00.00s'
+    fieldNums : DataFrame
+        Dataframe of field numbers associated with any two RA and Dec pair
+        
     Returns
     -------
     Integer
-        Field ID closest to given RA Dec
+        Associated field ID number for ra and dec pair
     '''
-    if decimal==False: #if RA Dec in 00h00m00s format, convert to decimal
-        RA,Dec = Angle(RA), Angle(Dec) #Converting ra and dec to angles
-        RA,Dec = float(RA.to_string(unit = u.degree, decimal = True)), float(Dec.to_string(unit = u.degree, decimal = True))
-    #import data and create a DataFrame
-    data=pd.read_fwf(file)
-    df=pd.DataFrame(data)
-    #find rows with Decs closest to desired Dec
-    decs=df['Dec'].between(abs(Dec)-sigma,abs(Dec)+sigma)
-    absDecs=df[decs]
-    #the data imported such that the negative values do not show up as negative, so this loop ensures that, if desired Dec is negative/positive, it gets the proper results
-    if Dec>0:
-        while absDecs.iloc[0,0]>302864: #positive Decs should have an ID less than this
-            absDecs=absDecs.drop([absDecs.iloc[0,0]-1])
-    else:
-        while absDecs.iloc[0,0]<302864: #negative Decs should have an ID less than this
-            absDecs=absDecs.drop([absDecs.iloc[0,0]-1])
-    smallestDist=360 #max distance it can be is 360, so this is the initial smallest
-    for row in range(len(absDecs)): #find closest RA by comparing each distance and saving when it is the new shortest
-        if abs(RA-absDecs.iloc[row,1])<smallestDist:
-            smallestDist=abs(RA-absDecs.iloc[row,1])
-            bestRow=row
-    return absDecs.iloc[bestRow,0]
+    
+    ra,dec = Angle(ra,unit=u.deg), Angle(dec,unit=u.deg) #Converting ra and dec to angles
+    ra,dec = float(ra.to_string(unit = u.degree, decimal = True)), float(dec.to_string(unit = u.degree, decimal = True))
+    
+    #print(ra,dec)
+    
+    closest_dec = float(fieldNums.iloc[(fieldNums['Dec']-dec).abs().argsort()[:1]]["Dec"])
+    narrowedDf = fieldNums[fieldNums.Dec == closest_dec]
+    
+    return int(narrowedDf.loc[[narrowedDf["RA"].sub(ra).abs().idxmin()]]["# ID"])
 
 
+data = pd.read_fwf("SUMMER_fields.txt")
+df = pd.DataFrame(data)
 
 def getRADecs(url):
     '''
@@ -88,41 +105,10 @@ def getRADecs(url):
         splitDec=decCol.split(':')
         Decs.append(splitDec[0]+'d'+splitDec[1]+'m'+splitDec[2]+'s')
     return RAs, Decs
-    
 
-def get_fields(url=None,RAs=None,Decs=None,sigma=0.13009,file='SUMMER_fields.txt',names=['ID',"RA",'Dec','EBV','GalLong','GalLat','EclLong','EclLat']):
-    '''
-    
-    Either have a url with the RA/Decs, or input your own through RAs and Decs.
-    Parameters
-    ----------
-    url : String
-        A URL of the supernovae from a csv in https://sites.astro.caltech.edu/ztf/bts/explorer.php?f=s&subsample=sn&classstring=Ia&quality=y&purity=y&ztflink=lasair&startsavedate=2021-05-31&endsavedate=2021-06-15&endpeakmag=19.0&reverse=y.RIPTION. Defaults to none if you have no url
-    RAs: List of strings
-        List of RAs in form 00h00m00s. Defaults to None. Leave as none if using a url for RA/Decs
-    Decs: List of strings
-        List of Decs in form 00d00m00s. Defaults to None. Leave as none if using a url for RA/Decs
-    sigma : FLoat, optional
-        Distance that the field's Dec can be from desired Dec. Defaults to 0.13009
-    file : String, optional
-        File name of file with fields. Defaults to "SUMMER_fields.txt
-    names : List of strings, optional
-        Names of columns. Defaults to ['ID',"RA",'Dec','EBV','GalLong','GalLat','EclLong','EclLat'])
-
-    Returns
-    -------
-    fields : List of integers
-        Field IDs of each RA/Dec
-
-    '''
-    if RAs==None and Decs==None:  
-        RAs,Decs=getRADecs(url)
-    fields=[]
-    for i in range(len(RAs)):
-        fields.append(fieldFinder(RAs[i],Decs[i]))
-    return fields
-                
-        
-    
-    
+def get_html_panda(URL):
+    html = requests.get(URL).content
+    df_list = pd.read_html(html)
+    df = df_list[-1]
+    return df['R.A.'],df['Decl.']
     
