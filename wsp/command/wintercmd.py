827cbaed2b22481e4d8d011c0fff77c077b763dd
Abigail Schipper <abbies@mit.edu>
2021-07-20 17:24:39 -0400

    Fixed brackets

--- wsp/command/wintercmd.py
+++ wsp/command/wintercmd.py
@@ -292,7 +292,7 @@ class Wintercmd(QtCore.QObject):
                 #image_data = fits.getdata(image_file)
                 #mean=np.mean(image_data[0:-1])
             for i in range(numPics):
-                print(('mount_goto_alt_az ')+str(self.config('flats_dither_alt'[i]))+' '+str(self.config(['flats']['dither_az'][i])))
+                print(('mount_goto_alt_az ')+str(self.config['flats_dither_alt'][i])+' '+str(self.config['flats']['dither_az'][i]))
                 #self.do('mount_goto_alt_az ')+str(self.config(['flats']['dither_alt'][i]))+' '+str(self.config(['flats']['dither_az'][i]))
                 print('ccd_do_exposure')
                 #self.do('ccd_do_exposure')
