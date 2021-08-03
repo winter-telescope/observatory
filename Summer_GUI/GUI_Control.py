3d14d781de57f9988393fb58e6a5f9b4248b573a
Josh Glass <joshglas@mit.edu>
2021-08-02 22:35:16 -0400

    gui improvements

--- Summer_GUI/GUI_Control.py
+++ Summer_GUI/GUI_Control.py
@@ -12,7 +12,7 @@ except:
     from PySide6.QtCore import QFile
     from PySide6.QtCore import QIODevice
     QT = 'PySide6'
-
+import qdarkstyle
 import time
 import socket
 import os
@@ -28,10 +28,7 @@ sys.path.insert(1, wsp_path)
 """
 This bit handles the update of the dictionary table in a nice HTML
 format, and saves the scrollbar position, setting the scrollbar back
-where it was after the text updates. There is a slight deviation
-downwards from the initial position because of rounding, but this
-rounding is necessary to set the positional value, and it's not
-very disruptive, so I've deemed it 'good enough'.
+where it was after the text updates.
 """
 
 def print_html_table_state(state):
@@ -40,7 +37,7 @@ def print_html_table_state(state):
     old_pos = vsb.value()
     window.output_display_2.setHtml(df.to_html(header=False))
     vsb.setValue(old_pos)
-
+timer_start = False
 class StateGetter(QtCore.QObject):
 
     """
@@ -104,6 +101,7 @@ def timer_handlings():
     except Exception:
         timer_start = False
         update_timer.stop()
+        return
     window.ccd_temp_display.display(state['ccd_tec_temp'])
     if state['ccd_tec_status'] == 1:
         change_ccd_indicator_green()
@@ -131,9 +129,18 @@ def timer_handlings():
 
 def send(cmd):
     # now that the connection is established, data can be sent with sendall() and received with recv()
-    sock.sendall(bytes(cmd,"utf-8"))
-    reply = sock.recv(1024).decode("utf-8")
-    window.output_display.appendPlainText(f"received message back from server: '{reply}'\n")
+    try:
+        sock.sendall(bytes(cmd,"utf-8"))
+        reply = sock.recv(1024).decode("utf-8")
+        window.output_display.appendPlainText(f"received message back from server: '{reply}'\n")
+        if cmd == 'chiller_start':
+            window.server_connect_button.setStyleSheet("background-color:green;")
+            window.server_connect_button.setText("WINTER Connected")
+    except socket.error:
+        sock.close()
+        window.output_display.appendPlainText(f"WSP has disconnected. Socket is closed until a manual reconnect. Did not send the command {cmd}")
+        window.server_connect_button.setStyleSheet("background-color:black;")
+        window.server_connect_button.setText("Connect to WSP")
 def test():
     print('please')
 
@@ -147,7 +154,7 @@ def connect_to_server():
         server_address = ('localhost', 7000)
         sock.connect(server_address)
         window.server_connect_button.setStyleSheet("background-color:green;")
-        window.server_connect_button.setText("WINTER Connected")
+        window.server_connect_button.setText("WSP Connected")
         if timer_start == False:
             # init the state getter
             monitor = StateGetter()
@@ -159,55 +166,47 @@ def connect_to_server():
         window.output_display.appendPlainText("Could not connect to WSP")
         sock.close()
 def chiller_start():
-    if timer_start == True:
-        send('chiller_start')
-        window.chiller_button.setStyleSheet("background-color:green;")
-        window.chiller_button.setText("Chiller Started")
+    send('chiller_start')
+
 def run_shutdown_script():
-    if timer_start == True:
-        send('total_shutdown')
-    else:
-        window.output_display.appendPlainText("Could not initiate shutdown sequence, not connected to WSP")
+    send('total_shutdown')
+
 def run_startup_script():
-    if timer_start == True:
-        # get the current housekeeping state
-        monitor.update_state()
-        monitor.print_state()
-        state = monitor.state
-        if monitor.state['ccd_tec_temp'] < 15:
-            send('total_startup')
-    else:
-        window.output_display.appendPlainText("Could not initiate startup sequence, not connected to WSP")
+    send('total_startup')
 
 def run_restart_script():
     restart_type = window.restart_selection.currentText()
     if restart_type == 'Total Restart':
-        window.output_display.appendPlainText("Total Restart")
+        send('total_restart')
     if restart_type == 'Mount Restart':
         window.output_display.appendPlainText("Mount Restart")
     if restart_type == 'Dome Restart':
         window.output_display.appendPlainText("Dome Restart")
 def do_exposure_script():
-    filter_selection = window.filter_selection.currentText()
-    if filter_selection == 'U Band Filter':
-        wheel = 1
-    if filter_selection == 'R Band Filter':
-        wheel = 3
-    ra = window.RA_input.text()
-    dec = window.DEC_input.text()
     exp = window.exposure_input.text()
-    send('command_filter_wheel ' + str(wheel))
-    while monitor.state['filter_wheel_position'] != str(wheel):
-        time.sleep(1)
-    send('ccd_set_exposure '+ exp)
-    while monitor.state['ccd_exptime'] != str(exp):
-        time.sleep(1)
-    window.output_display.appendPlainText("Taking a " + exp + " second exposure with " + filter_selection + " at coordinates " + ra + " , " + dec)
-    send('ccd_do_exposure')
+    if exp == "":
+        window.output_display.appendPlainText("Enter an exposure time")
+        return
+    if timer_start == True:
+        filter_selection = window.filter_selection.currentText()
+        if filter_selection == 'U Band Filter':
+            wheel = 1
+        if filter_selection == 'R Band Filter':
+            wheel = 3
+
+        send('command_filter_wheel ' + str(wheel))
+        while monitor.state['filter_wheel_position'] != wheel:
+            time.sleep(1)
+        send('ccd_set_exposure '+ exp)
+        while monitor.state['ccd_exptime'] != float(exp):
+            time.sleep(1)
+        send('ccd_do_exposure')
+    else:
+        window.output_display.appendPlainText("Could not take an exposure, not connected to WSP")
+
 def goto_coordinate_script():
     ra = window.RA_input.text()
     dec = window.DEC_input.text()
-    window.output_display.appendPlainText("Moved to " + ra + " , " + dec)
     send('mount_goto_ra_dec_j2000 ' + ra + " " + dec)
 
 def change_chiller_indicator_red():
@@ -230,25 +229,29 @@ def change_ccd_indicator_green():
     
 def toggle_dome_tracking():
     if window.dome_tracking_toggle.value() == 1:
-        window.output_display.appendPlainText("Dome Tracking On")
         send('dome_tracking_on')
     else:
-        window.output_display.appendPlainText("Dome Tracking Off")
         send('dome_tracking_off')
 def toggle_mount_tracking():
     if window.mount_tracking_toggle.value() == 1:
-        window.output_display.appendPlainText("Mount Tracking On")
         send('mount_tracking_on')
     else:
-        window.output_display.appendPlainText("Mount Tracking Off")
         send('mount_tracking_off')
+def observe_object():
+    send('robo_observe_object ' + window.object_entry.text())
 
+def focus_loop():
+    if window.plot_option.isChecked() == True:
+        send('doFocusLoop plot')
+    else:
+        send('doFocusLoop')
 def command_entry():
     send(window.command_entry.text())
 # In this section, the application is started, and the methods of the different widgets are linked to functions.
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
     ui_file_name = "form.ui"
     if QT == 'PySide6':
         ui_file = QFile(ui_file_name)
@@ -288,5 +291,7 @@ if __name__ == "__main__":
     window.dome_tracking_toggle.sliderReleased.connect(toggle_dome_tracking)
     window.mount_tracking_toggle.sliderReleased.connect(toggle_mount_tracking)
     window.command_execute.pressed.connect(command_entry)
+    window.observe_object_button.pressed.connect(observe_object)
+    window.do_focus_loop.pressed.connect(focus_loop)
     window.update_button.pressed.connect(timer_handlings)
     sys.exit(app.exec())
