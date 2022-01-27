4c85e0b5401a9dbbb89145441549922e4bbf810a
WINTER User <nate.lourie@gmail.com>
2022-01-27 12:29:49 -0800

    daily check for schedule creation that alerts scheduler group

---
+++ scheduler/check_for_schedule.py
@@ -0,0 +1,36 @@
+import yaml
+import sys
+from datetime import datetime
+from os.path import exists
+
+wsp_path = '../wsp'
+sys.path.insert(0, wsp_path)
+from alerts import alert_handler
+from utils import utils
+
+schedulefile_name = f'nightly_{utils.tonight_local()}.db'
+schedule_path = '/home/winter/data/schedules/'+schedulefile_name
+
+file_exists = exists(schedule_path)
+
+if file_exists == True:
+    msg = f"Nightly schedule file exists at {schedule_path}"
+else:
+     msg = f"ERROR! No nightly schedule file exist at {schedule_path}"
+
+auth_config_file  = wsp_path + '/credentials/authentication.yaml'
+user_config_file = wsp_path + '/credentials/alert_list.yaml'
+alert_config_file = wsp_path + '/config/alert_config.yaml'
+
+auth_config  = yaml.load(open(auth_config_file) , Loader = yaml.FullLoader)
+user_config = yaml.load(open(user_config_file), Loader = yaml.FullLoader)
+alert_config = yaml.load(open(alert_config_file), Loader = yaml.FullLoader)
+
+alertHandler = alert_handler.AlertHandler(user_config, alert_config, auth_config)
+
+#print(msg)
+  
+#alertHandler.email_group('scheduler', 'subject', msg)
+alertHandler.slack_message_group('scheduler', msg)
+
+
