ece8c76856dabf2291034b13753ca9ce2ad6e581
nlourie@mit.edu <nlourie@mit.edu>
2022-06-03 15:26:18 -0400

    version for test on pi

--- wsp/viscam/viscamd.py
+++ wsp/viscam/viscamd.py
@@ -13,3 +13,260 @@ raspberry pi to control the summer filter wheel, summer shutter, and other
 @author: winterpi
 """
 
+import requests
+import os
+import Pyro5.core
+import Pyro5.server
+from PyQt5 import QtCore
+import numpy as np
+import sys
+import signal
+import threading
+import time
+import logging
+import json
+from datetime import datetime
+
+# add the wsp directory to the PATH
+wsp_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(1, wsp_path)
+print(f'wsp_path = {wsp_path}')
+
+
+
+class CommandHandler(QtCore.QObject):
+    
+    newReply = QtCore.pyqtSignal(str)
+    #newCommand = QtCore.pyqtSignal(object)
+    newRequest = QtCore.pyqtSignal(object)
+    
+    def __init__(self, config, logger = None, verbose = False):
+        super(CommandHandler, self).__init__()
+        
+    def doCommand(self, cmd_obj):
+            """
+            This is connected to the newRequest signal. It parses the command and
+            then executes the corresponding command from the list below
+
+            using this as a reference: (source: https://stackoverflow.com/questions/6321940/how-to-launch-getattr-function-in-python-with-additional-parameters)     
+            
+            """
+            #print(f'dome: caught doCommand signal: {cmd_obj.cmd}')
+            cmd = cmd_obj.cmd
+            args = cmd_obj.args
+            kwargs = cmd_obj.kwargs
+            
+            try:
+                getattr(self, cmd)(*args, **kwargs)
+            except:
+                pass
+    
+    # send a command to the filter wheel
+    # -1: home the filter wheel (sends to pos 0)
+    # 1-6 - set filter wheel to position 1-6
+    # 8 - get current position
+    
+    def send_filter_wheel_command(self, position):
+        string = self.url + "filter_wheel?n=" + str(position)   
+        print(string)
+        try:
+            res = requests.get(string, timeout=10)
+            pos = int(res.text)
+            status = res.status_code
+            #print("Status", status, "Response: ", res.text)
+            #self.logger.info(f'Filter wheel status {status}, {res.text}')
+            self.fw_pos = pos
+            self.update_state()
+            return pos
+        except:
+            #print("Raspi is not responding")
+            self.log(f'Filter wheel is not responding')
+            return -11            
+                 
+                
+   
+    # send a command to the shutter
+    # 0 - close the shutter
+    # 1 - open the shutter    
+    def send_shutter_command(self, state):
+        string = self.url + "shutter?open=" + str(state)
+        try:
+            res = requests.get(string, timeout=10)
+            status = res.status_code
+            #print("Status", status, "Response: ", res.text)
+            self.log(f'Shutter status {status}, {res.text}')
+            return status
+        except:
+            #print("Raspi is not responding")
+            self.log(f'Shutter is not responding')
+            return 0
+    
+    # check what state the shutter is in
+    # CAUTION: this is a global variable in the web server,
+    # we cannot pull the actual shutter state
+    # do not trust this variable
+    # 0 - shutter is closed
+    # 1 - shutter is open
+    def check_shutter_state(self):
+        string = self.url  + "shutter_state"
+        try:
+            res = requests.get(string, timeout=10)
+            status = res.status_code
+            if res.text[0] == "3":
+                #print("Status: ", status, " Unknown shutter state")
+                return -1
+            elif res.text[0] == "1":
+                #print("Status: ", status, "Shutter state is ", res.text)
+                return 1
+            elif res.text[0] == "0":
+                #print("Status: ", status, "Shutter state is ", res.text)
+                return 0
+            else:
+                #print("Status", status, "Response: ", res.text)
+                #return status
+                return -1
+        except:
+            #print("Raspi is not responding")
+            return -1
+    
+        
+
+class CommandThread(QtCore.QThread):
+    newReply = QtCore.pyqtSignal(int)
+    newRequest = QtCore.pyqtSignal(object)
+    
+    def __init__(self, config, logger = None,  verbose = False):
+        super(QtCore.QThread, self).__init__()
+        
+        self.config = config
+        self.logger = logger
+        self.verbose = verbose
+    
+    def HandleRequest(self, request_object):
+        self.newRequest.emit(request_object)
+    
+    def DoReconnect(self):
+        #print(f'(Thread {threading.get_ident()}) Main: caught reconnect signal')
+        self.doReconnect.emit()
+    
+    def run(self):    
+        def SignalNewReply(reply):
+            self.newReply.emit(reply)
+        
+        self.commandHandler = CommandHandler(config = self.config, logger = self.logger, verbose = self.verbose)
+        # if the newReply signal is caught, execute the sendCommand function
+        self.newRequest.connect(self.commandHandler.doCommand)
+        self.commandHandler.newReply.connect(SignalNewReply)
+        
+        self.exec_()
+
+
+
+class Viscam(QtCore.QObject):
+    
+    
+    commandRequest = QtCore.pyqtSignal(object)
+
+    
+    def __init__(self,  config, URL, name = 'viscam', dt = 100, logger = None, verbose = False):
+        
+        super(Viscam, self).__init__()   
+        
+        self.config = config
+        self.url = URL
+        self.state = dict()
+        self.logger = logger
+        self.verbose = verbose
+        
+        self.commandThread = CommandThread(config = self.config, logger = self.logger, verbose = self.verbose)
+        # connect the signals and slots
+        
+        self.statusThread.start()
+        self.commandThread.start()
+        
+        # if the status thread is request a reconnection, trigger the reconnection in the command thread too
+        self.statusThread.doReconnect.connect(self.commandThread.DoReconnect)
+        
+        self.statusThread.newStatus.connect(self.updateStatus)
+        self.commandRequest.connect(self.commandThread.HandleRequest)
+        self.commandThread.newReply.connect(self.updateCommandReply)
+        self.log(f'chiller: running in thread {threading.get_ident()}')
+        
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(self.dt)
+        self.timer.timeout.connect(self.update)
+        self.timer.start()
+
+    
+    def setup_pdu_dict(self):
+        
+        # make a dictionary of all the pdus
+        for pduname in pdu_config['pdus']:
+            pduObj = pdu.PDU(pduname, self.pdu_config, self.auth_config, autostart = True, logger = logger)
+            
+            self.pdu_dict.update({pduname : pduObj})
+            
+        
+    def update(self):
+
+        for pduname in self.pdu_dict:
+            
+            # query the pdu status
+            pdustate = self.pdu_dict[pduname].getState()
+            self.state.update({pduname : pdustate})
+            #print(pdustate['status'][7])
+
+
+    def log(self, msg):
+        if self.logger is None:
+            print(f'Viscam: {msg}')
+        else:
+            self.logger.info(msg)
+    
+            
+    def update_state(self):
+        # poll the state, if we're not connected try to reconnect
+        # this should reconnect down the line if we get disconnected
+        """        
+        connected = self.send_raspi_check()
+        
+        if not connected:
+            self.state.update({'pi_status' : 1})
+            self.state.update({'pi_status_last_timestamp'  : datetime.utcnow().timestamp()})
+            
+        else:
+            self.state.update({'pi_status' : 1})
+            self.state.update({'pi_status_last_timestamp'  : datetime.utcnow().timestamp()})
+            
+
+            self.state.update({'filter_wheel_position' : self.fw_pos})
+            self.state.update({'filter_wheel_position_last_timestamp'  : datetime.utcnow().timestamp()})
+            
+            
+            shut = self.check_shutter_state()
+            self.state.update({'shutter_state' : shut})
+            self.state.update({'shutter_state_last_timestamp'  : datetime.utcnow().timestamp()})
+        """
+        
+    @Pyro5.server.expose
+    def getState(self):
+        #print(self.state)
+        return self.state 
+ 
+    
+    
+if __name__ == '__main__':
+    url = 'http://127.0.0.1:5001/'
+    viscam = Viscam(url, None)
+    
+    print()
+    print(f'FW Pos = {viscam.state.get("filter_wheel_position", -999)}')
+    print()
+    for i in range(5):
+        pos = np.random.randint(0,6)
+        print(f'FW: Sending to Pos = {pos}')
+        viscam.send_filter_wheel_command(pos)
+        time.sleep(1)
+        print(f'FW Pos = {viscam.state["filter_wheel_position"]}')
+        print()
+    
--- wsp/viscam/web_server.py
+++ wsp/viscam/web_server.py
@@ -6,27 +6,81 @@ Created on Wed May 12 11:14:15 2021
 @author: frostig
 """
 
-
+import json
 from flask import Flask
 from flask import request
 from flask.json import jsonify
+from datetime import datetime
 import FLI as fli
 import serial # pip install pyserial
 import time
 app = Flask(__name__)
 
 # https://stackoverflow.com/questions/19277280/preserving-global-state-in-a-flask-application
-shutter_state = {}
-shutter_state['foo'] = 3
+#shutter_state = {}
+#shutter_state['foo'] = 3
+
+STATE={}
 
 
 
+# init the shutter
+STATE.update({'shutter_open' : -1})             # init to unknown
+STATE.update({'shutter_status' : 0})            # init to not connected
+STATE.update({'shutter_response_code' : 0})     # init to okay
+"""
+# init the hardware
 try:
     FW = fli.USBFilterWheel.find_devices()[0]
 except:
     print('Exception: no filter wheel plugged in on start up')
 
-
+try:
+    SHUTTER_SER = serial.Serial('/dev/serial/by-id/usb-FTDI_MM232R_USB__-__Serial_A146VRZG-if00-port0', baudrate=9600, bytesize=8, parity='N')
+except:
+    print('could not connect to shutter')
+"""
+@app.route('/reconnect_fw')
+def reconnect_fw():
+    try:
+        FW = fli.USBFilterWheel.find_devices()[0]
+        FW.set_position(-1)
+        time.sleep(1)
+        pos = FW.get_position()
+        fw_status = 1
+        fw_response_code = 0
+    except:
+        fw_status = 0
+        pos = -9
+        fw_response_code = -4
+        
+    STATE.update({'fw_pos' : pos})                   # init to unknown
+    STATE.update({'fw_status' : fw_status})                 # init to not conn
+    STATE.update({'fw_response_code' : fw_response_code})          # init to okay
+    reply_string = json.dumps(STATE)
+    return reply_string
+    
+@app.route('/reconnect_shutter')
+def reconnect_shutter():
+    try:
+        SHUTTER_SER = serial.Serial('/dev/serial/by-id/usb-FTDI_MM232R_USB__-__Serial_A146VRZG-if00-port0', baudrate=9600, bytesize=8, parity='N')
+        
+        SHUTTER_SER.isOpen() # try to open port
+        shutter_open = -1
+        shutter_status = 1
+        shutter_response_code = 0
+        shutter_error = None
+    except Exception as e:
+        shutter_error = e
+        shutter_open = -1
+        shutter_status = 0
+        shutter_response_code = -4
+    STATE.update({'shutter_open' : shutter_open})
+    STATE.update({'shutter_status' : shutter_status})
+    STATE.update({'shutter_resonse_code' : shutter_response_code})
+    STATE.update({'shutter_error ' : shutter_error })
+    reply_string = json.dumps(STATE)
+    return reply_string
 # check raspi/web server responsiveness
 @app.route('/check_raspi', endpoint='func1', methods=['GET'])
 def check_raspi():
@@ -36,9 +90,18 @@ def check_raspi():
 # send a command to the filter wheel
 @app.route('/filter_wheel', endpoint='func2', methods=['GET'])
 def get_filter_command():
-    # open serial port
+    """
+    # Response code:
+        0  : command succeeds
+        -1 : command is unknown
+        -2 : parameter is bad
+        -3 : command cannot be executed at the current time
+        -4 : could not communicate with device
+    """
+        
     try:
-        #ser = serial.Serial('/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0')
+        # try to connect to FW
+        FW = fli.USBFilterWheel.find_devices()[0]
         #time.sleep(25)
         # get web request
         n = request.args.get("n")
@@ -50,29 +113,55 @@ def get_filter_command():
                 FW.set_filter_pos(n)
                 # if we get here it was successfully completed
                 pos = FW.get_filter_pos()
-                ret = pos
+                
+                fw_status = 1
+                fw_response_code = 0
+                
             except Exception as e:
                 print(f'error setting fw pos: {e}')
                 pos = -9
+                fw_status = 0
+                fw_response_code = -3
+                
+            STATE.update({'fw_pos' : pos})
+            STATE.update({'fw_status' : fw_status})
+            STATE.update({'fw_response_code' : fw_response_code})
+                
             #reply_string = "Set filter wheel position to " + str(n)
-            reply_string = str(pos)
+            #reply_string = str(pos)
+            reply_string = json.dumps(STATE)
+            
         elif (n == 8):
             # command 
             try:
                 # if we get here it was successfully completed
                 pos = FW.get_filter_pos()
                 #reply_string = "Current position is " + str(pos)
-                reply_string = str(pos)
+                #reply_string = str(pos)
+                fw_response_code = 0
             except:
                 pos = -9
+                fw_response_code = -3
                 #reply_string = "Could not  " + str(ret)
-                reply_string = str(pos)
+                #reply_string = str(pos)
+            STATE.update({'fw_pos' : pos})
+            STATE.update({'fw_status' : fw_status})
+            STATE.update({'fw_response_code' : fw_response_code})
+            
         else:
+            # not a valid command
+            fw_response_code = -2
             #reply_string = "Not a valid command"
             reply_string = str(-10)
+            STATE.update({'fw_pos' : pos})
+            STATE.update({'fw_status' : fw_status})
+            STATE.update({'fw_response_code' : fw_response_code})
+            reply_string = json.dumps(STATE)    
         return reply_string
     except:
-        reply_string = "Port could not be opened, try restarting the pi"
+        fw_response_code = -4
+        #reply_string = "Port could not be opened, try restarting the pi"
+        reply_string = json.dumps(STATE)
         return reply_string, 403
 
 
@@ -82,39 +171,74 @@ def get_shutter_command():
     # open serial port
     # see documentation for baudrate
     # https://www.uniblitz.com/wp-content/uploads/2020/10/VED24-User-Manual-1.41_Rev_B12.pdf
-    
+    shutter_cmd_recv_timestamp = datetime.utcnow().timestamp()
     try:
-        ser = serial.Serial('/dev/serial/by-id/usb-FTDI_MM232R_USB__-__Serial_A146VRZG-if00-port0', baudrate=9600, bytesize=8, parity='N')
+        #ser = serial.Serial('/dev/serial/by-id/usb-FTDI_MM232R_USB__-__Serial_A146VRZG-if00-port0', baudrate=9600, bytesize=8, parity='N')
     
         n = request.args.get("open")
         n = int(n)
         reply_string = ""
         if (n == 0):
-            # command
-            ser.write(b'A')
-            shutter_state['foo'] = 0
-            reply_string = "Close shutter" 
+            # CLOSE SHUTTER
+            # send command
+            SHUTTER_SER.write(b'A')
+            #shutter_state['foo'] = 0
+            shutter_open = 0
+            shutter_status = 1
+            shutter_response_code = 0
+            shutter_last_close_timestamp = datetime.utcnow().timestamp()
+            shutter_cmd_latency = shutter_last_close_timestamp - shutter_cmd_recv_timestamp
+            #reply_string = "Close shutter" 
         elif n == 1:
-            # command 
+            # OPEN SHUTTER
+            # send command 
             command = str(n) # make string
             command = command.encode() # make bytes
-            shutter_state['foo'] = 1
-            ser.write(b'@')
-            reply_string = "Open shutter"
+            #shutter_state['foo'] = 1
+            #shutter_state = 1
+            
+            SHUTTER_SER.write(b'@')
+            shutter_open = 1
+            shutter_status = 1
+            shutter_response_code = 0
+            shutter_last_open_timestamp = datetime.utcnow().timestamp()
+            shutter_cmd_latency = shutter_last_open_timestamp - shutter_cmd_recv_timestamp
+            
+            #reply_string = "Open shutter"
         else:
-            reply_string = "Not a valid command"
+            #reply_string = "Not a valid command"
+            shutter_response_code = -1
         
+        shutter_status = 1
+        STATE.update({'shutter_open' : shutter_open})
+        STATE.update({'shutter_status' : shutter_status})
+        STATE.update({'shutter_response_code' : shutter_response_code})
+        reply_string = json.dumps(STATE)
         return reply_string
+        
     except:
-        reply_string = "Port could not be opened"
+        # Port could not be opened
+        #reply_string = "Port could not be opened"
+        shutter_status = 0
+        shutter_response_code = -4
+        STATE.update({'shutter_open' : shutter_open})
+        STATE.update({'shutter_status' : shutter_status})
+        STATE.update({'shutter_response_code' : shutter_response_code})
+        reply_string = json.dumps(STATE)
         return reply_string, 403
-    
+
+
+
+"""    
 # send a command to the shutter
 @app.route('/shutter_state', endpoint='func4', methods=['GET'])
 def get_shutter_state():
     # check shutter state
     #reply_string = str(shutter_state)
     return jsonify(shutter_state['foo'])
-
+"""
 if __name__ == '__main__':
+    reconnect_fw()
+    time.sleep(5)
+    reconnect_shutter()
     app.run(host='0.0.0.0', port=5001, debug=True)
