312726f7747550d254bdca3a25a44d4c5a4aff58
winter@freya <nate.lourie@gmail.com>
2023-02-24 10:30:20 -0500

    testing socket conns

---
+++ development/pyro/network_comms_server_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 15:34:11 2023
+
+@author: winter
+"""
+
+import Pyro5.core
+import Pyro5.server
+
+class Hello(object):
+
+    def __init__ (self):
+        self.word = 'Hello'
+    @Pyro5.server.expose
+    def hello(self):
+        return(self.word)
+
+obj = Hello()
+name = 'hello'
+heimdall = '192.168.1.10'
+ns = Pyro5.core.locate_ns(host = heimdall)
+#%%
+freya = '192.168.1.20'
+daemon = Pyro5.server.Daemon(host = freya)
+uri = daemon.register(obj)
+ns.register(name, uri)
