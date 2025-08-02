b8e1e8cd0869e590950f3af66ef4882527322486
Nathan Lourie <nlourie@mit.edu>
2025-08-01 19:09:45 -0700

    trying to fix bad behavior when the mirror cover is unpowered that causes wsp to crash

--- wsp/telescope/mirror_cover.py
+++ wsp/telescope/mirror_cover.py
@@ -7,21 +7,22 @@ Created on Sun Jun 13 01:42:46 2021
 """
 
 
-
-
-import socket
 import io
+import socket
 import time
 from datetime import datetime
 
-
-MIRROR_COVER_STATUS_DICT = dict({0  : 'OPEN',
-                                 1  : 'CLOSED',
-                                 2  : 'OPENING',
-                                 3  : 'CLOSING',
-                                 4  : 'ERROR',
-                                 5  : 'STUCK',
-                                 255: 'CONNERR'})
+MIRROR_COVER_STATUS_DICT = dict(
+    {
+        0: "OPEN",
+        1: "CLOSED",
+        2: "OPENING",
+        3: "CLOSING",
+        4: "ERROR",
+        5: "STUCK",
+        255: "CONNERR",
+    }
+)
 
 
 class MirrorCovers:
@@ -36,56 +37,74 @@ class MirrorCovers:
         self.connected = False
         self.open_mirror_cover_socket(self.addr, self.port)
 
-        
     def open_mirror_cover_socket(self, addr, port):
         self.last_reconnect_timestamp = datetime.utcnow().timestamp()
-        self.logger.info(f'MirrorCover: Attempting to connect to PWShutter TCP server at address {addr} and port {port}')
+        self.logger.info(
+            f"MirrorCover: Attempting to connect to PWShutter TCP server at address {addr} and port {port}"
+        )
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.settimeout(0.5)
         try:
             self.sock.connect((addr, port))
             self.connected = True
-            self.logger.info(f'MirrorCover: Successfully opened socket connection to mirror cover!')
+            self.logger.info(
+                f"MirrorCover: Successfully opened socket connection to mirror cover!"
+            )
         except:
             self.connected = False
-            
+
     def update_state(self):
         now_timestamp = datetime.utcnow().timestamp()
-
         if self.connected:
             """
             (code, text) = self.sendreceive("isconnected")
             #print(f'mirror code for isconnected: {code}, {text}')
-            if (code == 0 or code == 1):  
+            if (code == 0 or code == 1):
                 self.state.update({'mirror_cover_connected' : int(not(code))})
                 self.state.update({'mirror_cover_connected_last_timestamp'  : datetime.utcnow().timestamp()})
             elif code == 255:
                 self.state.update({'mirror_cover_connected' : code})
                 self.logger.info(f'Error while connecting to mirror cover, code {code} ')
-                self.state.update({'mirror_cover_connected_last_timestamp'  : datetime.utcnow().timestamp()})    
+                self.state.update({'mirror_cover_connected_last_timestamp'  : datetime.utcnow().timestamp()})
             """
-            (code, text) = self.sendreceive( "shutterstate")
-            if (code == 0 or code == 1 or code == 2 or code == 3 or code == 4 or code == 5 or code == 255):  
-                self.state.update({'mirror_cover_state' : code})
-                
-                self.state.update({'mirror_cover_state_last_timestamp'  : now_timestamp})
-                self.state.update({'mirror_cover_connected' : self.connected})
-                self.state.update({'mirror_cover_connected_last_timestamp': now_timestamp})
+            (code, text) = self.sendreceive("shutterstate")
+            if (
+                code == 0
+                or code == 1
+                or code == 2
+                or code == 3
+                or code == 4
+                or code == 5
+                or code == 255
+            ):
+                self.state.update({"mirror_cover_state": code})
+                self.state.update({"mirror_cover_state_last_timestamp": now_timestamp})
+                self.state.update({"mirror_cover_connected": self.connected})
+                self.state.update(
+                    {"mirror_cover_connected_last_timestamp": now_timestamp}
+                )
+                self.state.update(
+                    {
+                        "mirror_cover_state_str": MIRROR_COVER_STATUS_DICT.get(
+                            code, "ERROR"
+                        )
+                    }
+                )
+            else:
+                self.state.update({"mirror_cover_state": code})
+                self.state.update({"mirror_cover_state_str": "ERROR"})
+                self.state.update({"mirror_cover_connected": self.connected})
         else:
-            # close and reset the socket
-            self.connected = False
-            self.state.update({'mirror_cover_connected' : self.connected})
-            # try to reconnect as long as its been a few seconds since the last connection attempt (ie don't flood the port)
-            if now_timestamp - self.last_reconnect_timestamp > 5.0:
-                self.sock.close()
-                self.open_mirror_cover_socket(self.addr, self.port)
-            pass
-            
-        
-        self.state.update({'mirror_cover_state_str' : MIRROR_COVER_STATUS_DICT.get(code, 'ERROR')})
-        
-        
-        
+            # Handle the case when not connected
+            self.state.update(
+                {
+                    "mirror_cover_connected": False,
+                    "mirror_cover_connected_last_timestamp": now_timestamp,
+                    "mirror_cover_state": 255,
+                    "mirror_cover_state_str": "ERROR",
+                }
+            )
+
     def readline(self):
         """
         Utility function for reading from a socket until a
@@ -95,10 +114,9 @@ class MirrorCovers:
         while True:
             data = self.sock.recv(1).decode("utf-8")
             buf.write(data)
-            if data == '\n':
+            if data == "\n":
                 return buf.getvalue()
-            
-    
+
     def sendreceive(self, command):
         """
         Send a command to the server, and read the response.
@@ -106,7 +124,7 @@ class MirrorCovers:
         Returns a tuple (code, error_text)
         """
         try:
-            self.sock.send(bytes(command + "\n","utf-8"))
+            self.sock.send(bytes(command + "\n", "utf-8"))
             response = self.readline()
             # The response should consist of a numeric code, optionally
             # followed by some text if the code is 255 (error). Parse this out.
@@ -119,6 +137,6 @@ class MirrorCovers:
         except:
             # could not communicate
             response_code = -1
-            error_text    = ""
+            error_text = ""
 
         return (response_code, error_text)
